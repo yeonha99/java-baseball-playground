6492c8240ea566a27d87e578510726f128708d33
yeonha99 <rhdqn1207@naver.com>
2024-04-28 14:35:15 +0900

    학습테스트 실습

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,2 +1,43 @@
-package study;public class SetTest {
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void setSize(){
+        assertThat(numbers.size()).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(Integer number){
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true","2,true","3,true","4,false","5,false"})
+    void containsWithFail(Integer number, boolean expected){
+        assertThat(numbers.contains(number)).isEqualTo(expected);
+    }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,38 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String targetString = "(1,2)";
+        int startIndex = targetString.indexOf("(") + 1;
+        int endIndex = targetString.indexOf(")");
+        String actual = targetString.substring(startIndex, endIndex);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("chatAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException가 발생한다.")
+    void chatAt() {
+        String targetString = "abc";
+        assertThatThrownBy(
+                () -> targetString.charAt(targetString.length())
+        ).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
